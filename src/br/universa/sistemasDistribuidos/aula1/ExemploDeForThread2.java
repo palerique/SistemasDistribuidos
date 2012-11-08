2abffc48c53ddf220a7f9b8b6f68156f2daf3d99
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-11-08 20:33:20 -0200

    Exercícios de Thread Aula 1
    Signed-off-by: Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>

---
+++ src/br/universa/sistemasDistribuidos/aula1/ExemploDeForThread2.java
@@ -0,0 +1,34 @@
+package br.universa.sistemasDistribuidos.aula1;
+
+import java.util.Date;
+
+public class ExemploDeForThread2 extends Thread {
+
+	public ExemploDeForThread2(String nome) {
+
+		super(nome);
+		start();
+	}
+
+	public void run() {
+
+		Date inicial = new Date();
+
+		for (int i = 0; i < 1000; i++) {
+
+			System.out.println(getName() + " Número sequencial: " + i);
+
+		}
+
+		System.out.println("Tempo de execução: "
+				+ (new Date().getTime() - inicial.getTime()));
+
+	}
+
+	public static void main(String[] args) {
+
+		new ExemploDeForThread2("A");
+		new ExemploDeForThread2("B");
+
+	}
+}
